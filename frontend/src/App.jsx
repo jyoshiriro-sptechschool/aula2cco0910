931282d3b1dbee010ff2c1b30b55e546ba1784ec
José Yoshiriro (SpTech) <jose.yoshihiro@bandtec.com.br>
2025-10-09 17:44:41 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          VOCÊ trabalha remoto no itaú e já clicou {count} vezes
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
