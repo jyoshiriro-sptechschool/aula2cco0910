4674f4fcde5817b7ac5e5428ea7bfca2c5b77bb2
José Yoshiriro (SpTech) <jose.yoshihiro@bandtec.com.br>
2025-10-23 21:37:32 -0300

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  // ver no log do terminal, não do navegador
+  console.log('IP_PORTA_API:', env.IP_PORTA_API)
+  
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.IP_PORTA_API || 'http://localhost:8080',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    }
+  }
 })
