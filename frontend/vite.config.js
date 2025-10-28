aa6ebf335b2134e2fe6b924583b4cfe6d41aeb25
José Yoshiriro (SpTech) <jose.yoshihiro@bandtec.com.br>
2025-10-28 10:30:20 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,15 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  
-  // ver no log do terminal, não do navegador
-  console.log('IP_PORTA_API:', env.IP_PORTA_API)
+export default defineConfig(() => {
   
   return {
     plugins: [react()],
     server: {
       proxy: {
         '/api': {
-          target: env.IP_PORTA_API || 'http://10.0.0.177:8080',
+          target: 'http://localhost:8080',
           changeOrigin: true,
           secure: false,
           rewrite: (path) => path.replace(/^\/api/, '')
